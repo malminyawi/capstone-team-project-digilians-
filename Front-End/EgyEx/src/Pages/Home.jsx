8b71c1e666e291d404c49d0bf64b70cb7cd7f792
shahdyasser126 <shahdyasser126@gmail.com>
2026-05-20 09:53:43 +0300

    add destanitions

--- Front-End/EgyEx/src/Pages/Home.jsx
+++ Front-End/EgyEx/src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import Hero from "../Components/Home/Hero/Hero";
-
+import Destanitions from "../Components/destinationsFeatures/Destanitions";
 export default function Home() {
   return (
     < >
       <Hero/>
+      <Destanitions/>
     </>
   )
 }
