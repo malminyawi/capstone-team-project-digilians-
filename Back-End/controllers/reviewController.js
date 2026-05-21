e5188bfa18b17547a78ebad0e696a9f3f270aa0d
Mariam <dahmashmariam@gmail.com>
2026-05-21 20:18:48 +0300

    review schema & create review

---
+++ Back-End/controllers/reviewController.js
@@ -0,0 +1,36 @@
+const Review = require("../models/Review.Models")
+const User = require("../models/User.models")
+const Trip = require("../models/Trips.Models")
+
+// creatr comment
+const createReview = async(req,res) => {
+    try{
+        const {desc, tripsId, userId, rate} = req.body 
+
+        const user = await User.findById(userId)
+
+        if(!user){
+            return res.status(403).json({message:"User Not  Auth"})
+        }
+
+        const trip = await Trip.findById(tripsId)
+        if(!trip){
+            return res.status(404).json({message:"Trip Not Found"})
+        }
+
+        //creat review 
+        const review = await Review.create({
+            desc,
+            user:userId,
+            trip:tripsId
+        })
+        res.status(200).json({message:"Review Created",review})
+        
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+
+}
+module.exports = {
+    createReview
+};
---
+++ Back-End/models/Review.Models.js
@@ -0,0 +1,34 @@
+
+const mongoose = require("mongoose")
+
+const reviewSchema = new mongoose.Schema({
+    fullName:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required: true
+    },
+    email:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required: true
+    },
+    rate:{
+        type:Number,
+        min:1,
+        max:5
+    },
+    trip:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"trips",
+        required:true
+    },
+    desc:{
+        type:String,
+        minlength:3,
+        required:true
+    }
+},{timestamps:true})
+
+const reviews = mongoose.model("Review",reviewSchema)
+
+module.exports = reviews
