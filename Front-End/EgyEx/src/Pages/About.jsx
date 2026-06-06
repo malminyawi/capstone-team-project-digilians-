bb18fa40a6deb26d1d56086219762abd5bc37b12
shahdyasser126 <shahdyasser126@gmail.com>
2026-06-06 11:07:55 +0300

    About Page

--- Front-End/EgyEx/src/Pages/About.jsx
+++ Front-End/EgyEx/src/Pages/About.jsx
@@ -1,10 +1,11 @@
-import { responsivePropType } from "react-bootstrap/esm/createUtilityClasses";
+ 
 
 import CardDetails from "../Components/Ui/CardDetails/CardDetails";
  
 import HeroAbout from "../Components/About/AboutPage/HeroAbout";
 import Details from "../Components/About/Details/Details";
 import Mission from "../Components/About/Mission/Mission";
+import Team from  "../Components/About/Team/Team";
 
 function About (){
     return(
@@ -13,7 +14,8 @@ function About (){
         
       <HeroAbout/>
         <Details/>
-       
+       <Mission/>
+       <Team/>
         </section> 
         </>
     )
